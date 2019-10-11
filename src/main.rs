a6a923dc73db79db921d420ce9565c0020d29972
OJIMA Ryoji <rojima1@gmail.com>
2019-10-11 15:39:23 +0900

    refactored.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use rayon::*;
-use rayon::prelude::*;
 use std::time::*;
 
 use stamp_art::beam_solver;
@@ -12,8 +10,6 @@ const DURATION: Duration = Duration::from_millis(9000);
 fn main() {
     let instant = Instant::now();
 
-    ThreadPoolBuilder::new().num_threads(2).build_global().unwrap();
-
     let (field, stamps, offsets) = read_question();
 
     let answer = {
@@ -21,24 +17,18 @@ fn main() {
         eprintln!("{}\t{}", result.len(), instant.elapsed().as_millis());
 
         let answers = [1, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192].iter().map(|beam_width| {
-            let answer = if *beam_width == 1 {
+            if *beam_width == 1 {
                 hill_climbing_solver::answer(field.clone(), &stamps, &instant, &DURATION)
             } else {
                 beam_solver::answer(field.clone(), &stamps, *beam_width, &instant, &DURATION)
-            };
-
-            if let Some(answer) = &answer {
-                eprintln!("{}\t{}\t{}", answer.len(), instant.elapsed().as_millis(), beam_width);
             }
-
-            answer
-        }).collect::<Vec<_>>();
+        }).take_while(|option| option.is_some()).map(|option| option.unwrap());
 
         for answer in answers {
-            if let Some(answer) = answer {
-                if answer.len() < result.len() {
-                    result = answer;
-                }
+            eprintln!("{}\t{}", answer.len(), instant.elapsed().as_millis());
+
+            if answer.len() < result.len() {
+                result = answer;
             }
         }
 
